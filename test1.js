47c0e3f67157581393aeac11a00ba50c9e4b57f6
UTamaD <37644187+UTamaD@users.noreply.github.com>
2018-03-22 11:17:02 +0900

    Update test1.js

--- test1.js
+++ test1.js
@@ -1,10 +1,25 @@
-function setup() {
-  createCanvas(640, 480);
-  stroke(255);
-  noFill();
+int i=0;
+
+
+void setup(){
+  size(512,512);
+  background(255);
+  frameRate(30);
 }
 
-function draw() {
-  background(0);
-  ellipse(mouseX, mouseY, 50, 50);  
+void draw(){
+  
+  if(i<width){
+    i=i+6;
+    stroke(0);
+    line(0,i,i,height);
+    line(width-0,height-i,width-i,height-height);
+    line(width,height-i,i,height);
+    line(0,i,width-i,0);
+    
+  }
+  
+  
+  
+  
 }
